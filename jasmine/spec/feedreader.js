5d58f1599ef516607ff9d230d53aa0c2d3454719
Ghaidairi <Ghaidairi@hotmail.com>
2017-11-01 00:18:01 +0300

    fix

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -100,5 +100,7 @@ $(function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
-         */
-}()});
+
+// describe & function */
+});
+});
